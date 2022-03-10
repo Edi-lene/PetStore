0f2ba0a74bc841a81f96a78bd3ba197b5df401c3
Edilene <edileneonogueira@gmail.com>
2022-03-10 11:29:50 -0300

    Finalizado o script excluitPet  metodo(Delete)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.contains;
@@ -68,4 +69,41 @@ public class Pet {
         System.out.println("O token é " + token);
 
     }
+    @Test(priority = 3)
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson("db/pet2.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .put(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Lopop"))
+                .body("status", is("sold"))
+        ;
+    }
+
+    @Test(priority = 4)
+    public void excluirPet() throws IOException {
+        String petId = "1983257";
+
+        given()
+                .contentType("application")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("massage", is(petId))
+        ;
+
+    }
+
 }
