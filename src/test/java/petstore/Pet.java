84b501cd5927cc2391f6a2c8dfa820ef7d200b4c
Edilene <edileneonogueira@gmail.com>
2022-03-10 10:19:41 -0300

    Completei o script consultarPet usando metodo(Get)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -15,20 +15,20 @@ import static org.hamcrest.Matchers.contains;
 // 3 - Classe
 public class Pet {
 // 3.1 - Atributos os adjetivos
-    String uri = "https://petstore.swagger.io/v2/pet"; // endere√ßo da entidade Pet
+    String uri = "https://petstore.swagger.io/v2/swagger.json"; // endereÁo da entidade Pet
 
-// 3.2 - Metodos( faz e n√£o volta retorno) e Fun√ß√µes fazem uma a√ß√£o devolve um resultado
+// 3.2 - Metodos( faz e n„o volta retorno) e FunÁıes fazem uma aÁ„o devolve um resultado
     public String lerJson(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
-    //incluir - Create - Post
-   @Test // Identifica o m√©todo ou fun√ß√£o como um teste para o TestNG
+    //Incluir - Create - Post
+   @Test (priority=1)// Identifica o mÈtodo ou funÁ„o como um teste para o TestNG
     public void incluirPet() throws IOException {
         String jsonBody = lerJson("db/pet1.json");
 
         // Sintaxe Gherkin
-        // Dado - Quando - Ent√£o
+        // Dado - Quando - Ent„o
        //  Given - When - Then
 
        given() //Dado
@@ -37,15 +37,35 @@ public class Pet {
                .body(jsonBody)
        .when() // Quando
                .post(uri)
-       .then() // Ent√£o
+       .then() // Ent„o
                .log().all()
                .statusCode(200)
-               .body("name", is("Lolipop"))
+               .body("name", is("Lopop"))
                .body("status", is("available"))
-               .body("category.name", is("dog"))
-               .body("tags.name", contains("status"))
+               .body("category.name", is("Adi7e"))
+               .body("tags.name", contains("data"))
        ;
     }
+    @Test(priority=2)
+    public void consultarPet(){
+        String petId= "1983257";
 
+        String token =
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Lopop"))
+                .body("category.name", is("Adi7e"))
+                .body("status", is("available"))
+        .extract()
+                .path("category.name")
+        ;
+        System.out.println("O token È " + token);
 
+    }
 }
